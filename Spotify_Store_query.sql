5a15044eaf542c541b8b3a5a179e43cce2645f86
Raju <96568750+vmaraju@users.noreply.github.com>
2023-09-06 15:44:18 +0530

    Add files via upload

---
+++ Spotify_Store_query.sql
@@ -0,0 +1,112 @@
+/* senior most employee based on job title */
+
+SELECT title, last_name, first_name 
+FROM employee
+ORDER BY levels DESC
+LIMIT 1
+
+
+/*  countries have the most Invoices */
+
+SELECT COUNT(*) AS c, billing_country 
+FROM invoice
+GROUP BY billing_country
+ORDER BY c DESC
+
+
+/* top 3 values of total invoice */
+
+SELECT total 
+FROM invoice
+ORDER BY total DESC
+
+
+/* city has the best customers  
+Writing a query that returns one city that has the highest sum of invoice totals. so returning both city and invoice total */
+
+SELECT billing_city,SUM(total) AS InvoiceTotal
+FROM invoice
+GROUP BY billing_city
+ORDER BY InvoiceTotal DESC
+LIMIT 1;
+
+
+/* the best customer- the person who has spent the most money.*/
+
+SELECT customer.customer_id, first_name, last_name, SUM(total) AS total_spending
+FROM customer
+JOIN invoice ON customer.customer_id = invoice.customer_id
+GROUP BY customer.customer_id
+ORDER BY total_spending DESC
+LIMIT 1;
+
+
+/* query to return the email, first name, last name, & Genre of all Rock Music listeners. */
+
+SELECT DISTINCT email,first_name, last_name
+FROM customer
+JOIN invoice ON customer.customer_id = invoice.customer_id
+JOIN invoiceline ON invoice.invoice_id = invoiceline.invoice_id
+WHERE track_id IN(
+	SELECT track_id FROM track
+	JOIN genre ON track.genre_id = genre.genre_id
+	WHERE genre.name LIKE 'Rock'
+)
+ORDER BY email;
+
+
+/*The artists who have written the most rock music in our dataset that 
+returns the Artist name and total track count of the top 10 rock bands. */
+
+SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
+FROM track
+JOIN album ON album.album_id = track.album_id
+JOIN artist ON artist.artist_id = album.artist_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+GROUP BY artist.artist_id
+ORDER BY number_of_songs DESC
+LIMIT 10;
+
+
+/*Return all the track names that have a song length longer than the average song length. 
+Returning the Name and Milliseconds for each track. */
+
+SELECT name,miliseconds
+FROM track
+WHERE miliseconds > (
+	SELECT AVG(miliseconds) AS avg_track_length
+	FROM track )
+ORDER BY miliseconds DESC;
+
+
+
+/* query that returns each country along with the top Genre. For countries where 
+the maximum number of purchases is shared return all Genres. */
+
+WITH popular_genre AS 
+(
+    SELECT COUNT(invoice_line.quantity) AS purchases, customer.country, genre.name, genre.genre_id, 
+	ROW_NUMBER() OVER(PARTITION BY customer.country ORDER BY COUNT(invoice_line.quantity) DESC) AS RowNo 
+    FROM invoice_line 
+	JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
+	JOIN customer ON customer.customer_id = invoice.customer_id
+	JOIN track ON track.track_id = invoice_line.track_id
+	JOIN genre ON genre.genre_id = track.genre_id
+	GROUP BY 2,3,4
+	ORDER BY 2 ASC, 1 DESC
+)
+SELECT * FROM popular_genre WHERE RowNo <= 1
+
+
+/* query that returns the country along with the top customer and how much they spent. 
+For countries where the top amount spent is shared, provide all customers who spent this amount. */
+
+WITH Customter_with_country AS (
+		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending,
+	    ROW_NUMBER() OVER(PARTITION BY billing_country ORDER BY SUM(total) DESC) AS RowNo 
+		FROM invoice
+		JOIN customer ON customer.customer_id = invoice.customer_id
+		GROUP BY 1,2,3,4
+		ORDER BY 4 ASC,5 DESC)
+SELECT * FROM Customter_with_country WHERE RowNo <= 1
